2585352c936134ac45cf7ef69bc24ca36af0a53b
rileyzzz <rileylemmler@gmail.com>
2023-01-11 03:22:11 -0600

    increment minor version

--- __init__.py
+++ __init__.py
@@ -20,7 +20,7 @@
 bl_info = {
     "name": "Export Indexed Mesh Format (.im)",
     "author": "Riley Lemmler",
-    "version": (1, 5, 2),
+    "version": (1, 5, 3),
     "blender": (2, 93, 0),
     "location": "File > Export",
     "description": "Export Trainz indexed meshes",
